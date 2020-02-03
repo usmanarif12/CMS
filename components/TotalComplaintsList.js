6616777e4d2ae75156928c69d09cd88d2ffb9efe
Shahbaz haider <shahbazhaider5297@gmail.com>
2020-02-03 09:49:55 -0800

    view Complaints

--- components/TotalComplaintsList.js
+++ components/TotalComplaintsList.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
   SafeAreaView,
   View,
@@ -7,319 +7,179 @@ import {
   Text,
   TouchableHighlight,
   Button,
+  ActivityIndicator,
+  AsyncStorage,
+  StatusBar
 } from 'react-native';
 import Constants from 'react-native';
-import {createStackNavigator} from 'react-navigation-stack';
-import {createAppContainer} from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
 
 import ViewComplaintsScreen from '../components/ViewComplaints.js';
-import {} from 'react-native-gesture-handler';
-import {Card} from 'react-native-shadow-cards';
+import { } from 'react-native-gesture-handler';
+import { Card } from 'react-native-shadow-cards';
 
 class TotalComplaintsList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      //defauilt value of the date time
-      date: '',
+
+dataSource:[],
+      isLoading: true,
     };
   }
 
-  componentDidMount() {
-    var that = this;
-    var date = new Date().getDate(); //Current Date
-    var month = new Date().getMonth() + 1; //Current Month
-    var year = new Date().getFullYear(); //Current Year
-    var hours = new Date().getHours(); //Current Hours
-    var min = new Date().getMinutes(); //Current Minutes
-    var sec = new Date().getSeconds(); //Current Seconds
-    that.setState({
-      //Setting the value of the date time
-      date: date + '/' + month + '/' + year + ' |',
+
+  
+
+  async componentDidMount() {
+    const userId = await AsyncStorage.getItem('userId');
+    return fetch('http://bsmartcms.com/cp/api/user-complains/' + userId)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      this.setState({
+        isLoading: false,
+        dataSource: responseJson.data,
+        
+      }, function() {
+        console.log(this.state.dataSource)
+      });
+    })
+    .catch((error) => {
+      console.error(error);
     });
+
+
+
   }
-  render() {
-    return (
-      <View style={styles.container}>
-        <View
+
+
+  renderItem = ({item}) => (
+
+    <View style={{ flex: 1, marginTop: Constants.statusBarHeight}}>
+      <View
+        style={{
+          width: '100%',
+          height: 1,
+          backgroundColor: '#e2e2e2',
+          marginTop: 5,
+        }}
+      />
+
+      <View style={{ flexDirection: 'row' }}>
+        <Text
           style={{
-            flexDirection: 'row',
-            marginTop: 20,
-            justifyContent: 'flex-end',
-            marginRight: 20,
-          }}></View>
+            fontSize: 16,
+            alignItems: 'center',
+            color: '#a9a9a9',
+            width: 100,
+            padding: 5,
 
-        <View
+            marginTop: 10,
+            marginLeft: 20,
+          }}>
+          {item.launch_date}
+        </Text>
+
+        <Text
           style={{
-            width: '100%',
-            height: 1,
-            backgroundColor: '#e2e2e2',
-            marginTop: 5,
-          }}
-        />
+            fontSize: 16,
+            alignItems: 'center',
+            color: '#a9a9a9',
+            width: 100,
+            padding: 5,
 
-        <View style={{flexDirection: 'row'}}>
-          <Text
-            style={{
-              fontSize: 16,
-              alignItems: 'center',
-              color: '#a9a9a9',
-              width: 100,
-              padding: 5,
+            marginTop: 10,
+            marginLeft: 20,
+          }}>
+          {item.nature_id}
+        </Text>
 
-              marginTop: 10,
-              marginLeft: 20,
-            }}>
-            {this.state.date}
-          </Text>
-
-          <Text
-            style={{
-              fontSize: 16,
-              alignItems: 'center',
-              color: '#a9a9a9',
-              width: 100,
-              padding: 5,
-
-              marginTop: 10,
-              marginLeft: 20,
-            }}>
-            Energy |
-          </Text>
-
-          <Text
-            style={{
-              fontSize: 16,
-              alignItems: 'center',
-              color: '#a9a9a9',
-              width: 100,
-              padding: 5,
-
-              marginTop: 10,
-              marginLeft: 10,
-            }}>
-            In Progress|
-          </Text>
-        </View>
-
-        <View
+        <Text
           style={{
-            width: '100%',
-            height: 1,
-            backgroundColor: '#e2e2e2',
-          }}
-        />
+            fontSize: 16,
+            alignItems: 'center',
+            color: '#a9a9a9',
+            width: 100,
+            padding: 5,
 
-        <TouchableHighlight
-          onPress={() =>
-            this.props.navigation.navigate('ViewComplaintsScreen')
-          }>
-          <View style={styles.item}>
-            <View style={{justifyContent: 'center', alignItems: 'center'}}>
-              <Text
-                style={{fontSize: 28, fontWeight: 'bold', color: '#57595d'}}>
-                #1
-              </Text>
-            </View>
-            <View
-              style={{
-                width: 1,
-                height: '100%',
-                backgroundColor: '#e2e2e2',
-                marginLeft: 15,
-              }}></View>
-            <View style={{marginLeft: 25}}>
-              <Text style={styles.title}>My AC is out of Freon</Text>
-              <Text style={styles.description}>
-                I need a person to repair my AC ASAP
-              </Text>
-            </View>
+            marginTop: 10,
+            marginLeft: 10,
+          }}>
+          {item.status}
+        </Text>
+      </View>
+
+      <View
+        style={{
+          width: '100%',
+          height: 1,
+          backgroundColor: '#e2e2e2',
+        }}
+      />
+
+      <TouchableHighlight
+        onPress={() =>
+          this.props.navigation.navigate('ViewComplaintsScreen')
+        }>
+        <View style={{
+          backgroundColor: '#fff',
+          padding: 20,
+          marginVertical: 8,
+          marginHorizontal: 16,
+          flexDirection: 'row',
+        }}>
+          <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+            <Text
+              style={{ fontSize: 28, fontWeight: 'bold', color: '#57595d' }}>
+              {item.id}
+            </Text>
           </View>
-        </TouchableHighlight>
-
-        <View
-          style={{
-            width: '100%',
-            height: 1,
-            backgroundColor: '#e2e2e2',
-          }}
-        />
-
-        <View style={{flexDirection: 'row'}}>
-          <Text
-            style={{
-              fontSize: 16,
-              alignItems: 'center',
-
-              width: 100,
-              padding: 5,
-              color: '#a9a9a9',
-              marginTop: 10,
-              marginLeft: 20,
-            }}>
-            {this.state.date}
-          </Text>
-
-          <Text
-            style={{
-              fontSize: 16,
-              alignItems: 'center',
-              color: '#a9a9a9',
-              width: 100,
-              padding: 5,
-
-              marginTop: 10,
-              marginLeft: 20,
-            }}>
-            Disaster |
-          </Text>
-
-          <Text
-            style={{
-              fontSize: 16,
-              alignItems: 'center',
-              color: '#a9a9a9',
-              width: 100,
-              padding: 5,
-
-              marginTop: 10,
-              marginLeft: 30,
-            }}>
-            Resolved |
-          </Text>
           <View
             style={{
-              width: '100%',
-              height: 1,
+              width: 1,
+              height: '100%',
               backgroundColor: '#e2e2e2',
-            }}
+              marginLeft: 15,
+            }}></View>
+          <View style={{ marginLeft: 25 }}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Text style={styles.description}>
+              {item.description}
+            </Text>
+          </View>
+        </View>
+      </TouchableHighlight>
+    </View>
+
+
+  );
+
+
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={{ marginTop: 10 }}>
+          <ActivityIndicator size="large"></ActivityIndicator>
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.Container}>
+          <StatusBar
+            backgroundColor="#2E8B57"
+            barStyle="light-content"></StatusBar>
+
+          <FlatList
+            data={this.state.dataSource}
+            renderItem={this.renderItem}
+            horizontal={false}
+
+            keyExtractor={(item, index) => index}
           />
         </View>
-        <View
-          style={{
-            width: '100%',
-            height: 1,
-            backgroundColor: '#e2e2e2',
-          }}
-        />
-
-        <TouchableHighlight
-          onPress={() =>
-            this.props.navigation.navigate('ViewComplaintsScreen')
-          }>
-          <View style={styles.item}>
-            <View style={{justifyContent: 'center', alignItems: 'center'}}>
-              <Text
-                style={{fontSize: 28, fontWeight: 'bold', color: '#57595d'}}>
-                #2
-              </Text>
-            </View>
-            <View
-              style={{
-                width: 1,
-                height: '100%',
-                backgroundColor: '#e2e2e2',
-                marginLeft: 15,
-              }}></View>
-            <View style={{flexDirection: 'column', marginLeft: 25}}>
-              <Text style={styles.title}>My home has been damaged</Text>
-              <Text style={styles.description}>
-                I need a person to repair my damaged home ASAP
-              </Text>
-              <View style={{justifyContent: 'center'}}></View>
-            </View>
-          </View>
-        </TouchableHighlight>
-
-        <View
-          style={{
-            width: '100%',
-            height: 1,
-            backgroundColor: '#e2e2e2',
-          }}
-        />
-
-        <View style={{flexDirection: 'row'}}>
-          <Text
-            style={{
-              fontSize: 16,
-              alignItems: 'center',
-              color: '#a9a9a9',
-              width: 100,
-              padding: 5,
-
-              marginTop: 10,
-              marginLeft: 20,
-            }}>
-            {this.state.date}
-          </Text>
-
-          <Text
-            style={{
-              fontSize: 16,
-              alignItems: 'center',
-              color: '#a9a9a9',
-              width: 100,
-              padding: 5,
-
-              marginTop: 10,
-              marginLeft: 20,
-            }}>
-            Bank |
-          </Text>
-
-          <Text
-            style={{
-              fontSize: 16,
-              alignItems: 'center',
-              color: '#a9a9a9',
-              width: 100,
-              padding: 5,
-
-              marginTop: 10,
-              marginLeft: 20,
-            }}>
-            In Progress|
-          </Text>
-        </View>
-        <View
-          style={{
-            width: '100%',
-            height: 1,
-            backgroundColor: '#e2e2e2',
-          }}
-        />
-
-        <TouchableHighlight
-          onPress={() =>
-            this.props.navigation.navigate('ViewComplaintsScreen')
-          }>
-          <View style={styles.item}>
-            <View style={{justifyContent: 'center', alignItems: 'center'}}>
-              <Text
-                style={{fontSize: 28, fontWeight: 'bold', color: '#57595d'}}>
-                #3
-              </Text>
-            </View>
-            <View
-              style={{
-                width: 1,
-                height: '100%',
-                backgroundColor: '#e2e2e2',
-                marginLeft: 15,
-              }}
-            />
-
-            <View style={{marginLeft: 25}}>
-              <Text style={styles.title}>My bank account is blocked.</Text>
-              <Text style={styles.description}>
-                Please verify my bank account details and unblock it
-              </Text>
-            </View>
-          </View>
-        </TouchableHighlight>
-      </View>
-    );
+      );
+    }
   }
 }
 
@@ -358,24 +218,24 @@ const styles = StyleSheet.create({
 const AppNavigator = createStackNavigator({
   TotalComplaintsList: {
     screen: TotalComplaintsList,
-    navigationOptions : {
+    navigationOptions: {
       title: 'Complaints List',
       headerStyle: {
         backgroundColor: '#2E8B57',
-        
+
       },
       headerTintColor: '#fff',
       headerTitleStyle: {
         fontWeight: 'bold',
-        marginLeft:10
+        marginLeft: 10
       },
     }
   },
   ViewComplaintsScreen: {
     screen: ViewComplaintsScreen,
     navigationOptions: {
-    headerShown:false
-  }
+      headerShown: false
+    }
   },
 });
 export default createAppContainer(AppNavigator);
